a4d8b018e44832c35d6c41efbe415959b7095857
Dylan SAMSON <dylansamson@live.fr>
2024-07-03 18:03:05 +0200

    Passage de puissance à conso pour userConsumptionAndForecasts

--- routes/data.js
+++ routes/data.js
@@ -12,10 +12,14 @@ router.get(
 
     try {
       const query = `
-      SELECT timestamp, Predictions, Actuals 
-      FROM hackaton2024.predict 
-      WHERE userID = '${userID}'
-    `;
+        SELECT timestamp, Predictions, Actuals 
+        FROM hackaton2024.predict 
+        WHERE userID = '${userID}'
+        ORDER BY timestamp
+      `;
+
+      // Log the query to debug
+      console.log("Executing query:", query);
 
       // Check if query is a string and not undefined
       if (typeof query !== "string") {
@@ -26,13 +30,29 @@ router.get(
       const row = await client.query({ query });
       const resultSet = await row.json();
 
-      const data = resultSet.data.map((row) => ({
-        timestamp: row.timestamp,
-        Predictions: row.Predictions,
-        Actuals: row.Actuals,
-      }));
+      console.log("Query result:", resultSet);
 
-      res.json({ data });
+      // Calculate consumption
+      let totalConsumption = 0;
+      let previousRow = null;
+
+      const data = resultSet.data.map((row) => {
+        if (previousRow) {
+          const timeDifference =
+            (new Date(row.timestamp) - new Date(previousRow.timestamp)) /
+            3600000;
+          const consumption = row.Actuals * timeDifference;
+          totalConsumption += consumption;
+        }
+        previousRow = row;
+        return {
+          timestamp: row.timestamp,
+          Predictions: row.Predictions,
+          Actuals: row.Actuals,
+        };
+      });
+
+      res.json({ data, totalConsumption });
     } catch (error) {
       console.error("Error executing query:", error);
       res.status(500).json({
