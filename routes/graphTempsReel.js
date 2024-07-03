579712aeffc4661b4b95576a2e46ab9fbcf09ecd
Dylan SAMSON <dylansamson@live.fr>
2024-07-03 16:10:11 +0200

    fix randomValues

--- routes/graphTempsReel.js
+++ routes/graphTempsReel.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const { randomValue } = require("../lib/random");
+const randomValue = require("../lib/random");
 const ensureAuthenticated = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/currentConsum", ensureAuthenticated, () => {
-  randomValue(200, 9000);
-  return randomValue;
+router.get("/currentConsum", ensureAuthenticated, (req, res, next) => {
+  currentConsum = randomValue(200, 9000);
+  res.json({ currentConsum });
 });
 
-router.get("/currentProd", ensureAuthenticated, () => {
-  randomValue(6000, 6900);
-  return randomValue;
+router.get("/currentProd", ensureAuthenticated, (req, res, next) => {
+  currentProd = randomValue(6000, 6900);
+  res.json({ currentProd });
 });
 
 module.exports = router;
