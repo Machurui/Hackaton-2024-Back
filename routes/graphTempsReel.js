d2038946d5d09ac56605287ef86b77e2d1fcc1f3
Dylan SAMSON <dylansamson@live.fr>
2024-07-03 16:16:29 +0200

    correction nom routes

--- routes/graphTempsReel.js
+++ routes/graphTempsReel.js
@@ -4,12 +4,12 @@ const ensureAuthenticated = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/currentConsum", ensureAuthenticated, (req, res, next) => {
-  currentConsum = randomValue(200, 9000);
-  res.json({ currentConsum });
+router.get("/currentPower", ensureAuthenticated, (req, res, next) => {
+  currentPower = randomValue(200, 9000);
+  res.json({ currentPower });
 });
 
-router.get("/currentProd", ensureAuthenticated, (req, res, next) => {
+router.get("/currentProduction", ensureAuthenticated, (req, res, next) => {
   currentProd = randomValue(6000, 6900);
   res.json({ currentProd });
 });
